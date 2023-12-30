f32dbe9333949350163a58a4235748ff9221ceeb
Omar Faruk <faruk.themefisher@gmail.com>
2023-12-30 12:18:37 +0600

    fixing fetch url

--- src/lib/fetchUser.ts
+++ src/lib/fetchUser.ts
@@ -1,14 +1,21 @@
+import config from "@/config/config.json";
 import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
+const { base_url } = config.site;
 export const fetchUserByEmail = async () => {
-  const userData = await fetch("http://localhost:3000/api/users", {
-    method: "GET",
-    headers: headers(),
-    cache: "no-cache",
-  });
+  const userData = await fetch(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/api/users"
+      : base_url + "/api/users",
+    {
+      method: "GET",
+      headers: headers(),
+      cache: "no-cache",
+    },
+  );
 
   // const data = await userData.json();
-  revalidatePath("/dashboard/subscriptions", "page");
+  revalidatePath("/", "page");
 
   return await userData.json();
 };
