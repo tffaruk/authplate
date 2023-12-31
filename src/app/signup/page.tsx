0c5834c00eeea7a66b45106e1521edb361fb928e
Omar Faruk <faruk.themefisher@gmail.com>
2023-12-31 16:52:57 +0600

    remove provider

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,10 @@
 import RegisterForm from "@/components/Form/RegistrationForm";
 import FormContainer from "@/components/FormContainer";
-import Provider from "@/components/Provider";
-import { getProviders } from "next-auth/react";
 import Link from "next/link";
 
 const SignUp = async () => {
-  const provider = await getProviders();
-  const authProviders = Object?.values(provider!);
+  // const provider = await getProviders();
+  // const authProviders = Object?.values(provider!);
   return (
     <FormContainer>
       <div className="text-center mb-12">
@@ -19,7 +17,7 @@ const SignUp = async () => {
         </p>
       </div>
       <RegisterForm />
-      <div className="relative w-full h-[1px] bg-[#B3B8C2] mb-4">
+      {/* <div className="relative w-full h-[1px] bg-[#B3B8C2] mb-4">
         <span className="absolute bg-theme-light z-10 inline-block left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 px-2">
           Or Continue With
         </span>
@@ -29,7 +27,7 @@ const SignUp = async () => {
           provider.type != "credentials" && (
             <Provider key={provider.id} provider={provider} />
           ),
-      )}
+      )} */}
     </FormContainer>
   );
 };
